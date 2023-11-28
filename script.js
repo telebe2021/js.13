bf565cc12281c1e2a61aa3a57eb0a326b38de301
telebe2021 <mahsungulmmmdov@gmail.com>
2023-11-28 14:51:44 +0400

    commit olundu

--- script.js
+++ script.js
@@ -1,47 +1,106 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const addButton = document.getElementById("btn");
-    const titleInput = document.getElementById("title");
-    const authorInput = document.getElementById("author");
-    const colorInput = document.getElementById("color");
-    const tableBody = document.getElementById("table");
-
-    addButton.addEventListener("click", function () {
-        if (titleInput.value === "") {
-            alert("Please enter a title!");
-            return;
-        }
-
-        const newRow = document.createElement("tr");
-
-        const titleCell = document.createElement("td");
-        titleCell.textContent = titleInput.value;
-
-        const authorCell = document.createElement("td");
-        authorCell.textContent = authorInput.value;
-
-        const typeCell = document.createElement("td");
-        typeCell.textContent = getType();
-
-        newRow.style.backgroundColor = colorInput.value;
-
-        newRow.appendChild(titleCell);
-        newRow.appendChild(authorCell);
-        newRow.appendChild(typeCell);
-
-        tableBody.appendChild(newRow);
-
-        titleInput.value = "";
-        authorInput.value = "";
-        colorInput.value = "";
+    const nav = document.createElement("nav");
+    const navContainer = document.createElement("div");
+    navContainer.className = "container";
+    const navLogo = document.createElement("div");
+    navLogo.className = "logo";
+    const navTitle = document.createElement("h1");
+    navTitle.textContent = "Start Bootstrap";
+    const navList = document.createElement("ul");
+    const navItems = ["home", "Resume", "Projects", "Contact"];
+    navItems.forEach(item => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.textContent = item;
+        li.appendChild(a);
+        navList.appendChild(li);
     });
 
-    function getType() {
-        const radioButtons = document.querySelectorAll(".radio");
-        for (const radioButton of radioButtons) {
-            if (radioButton.checked) {
-                return radioButton.nextElementSibling.textContent;
-            }
-        }
-        return "";
-    }
+    navLogo.appendChild(navTitle);
+    navLogo.appendChild(navList);
+    navContainer.appendChild(navLogo);
+    nav.appendChild(navContainer);
+    document.body.appendChild(nav);
+
+    const header = document.createElement("header");
+    const headerContainer = document.createElement("div");
+    headerContainer.className = "container";
+    const headerDiv = document.createElement("div");
+    headerDiv.className = "ikidiv";
+
+    const textDiv = document.createElement("div");
+    textDiv.className = "text";
+    const textH1 = document.createElement("h1");
+    textH1.textContent = "DESIGN · DEVELOPMENT · MARKETING";
+    const textP1 = document.createElement("p");
+    textP1.textContent = "I can help your business to";
+    const textH2 = document.createElement("h2");
+    textH2.textContent = "Get online and grow fast";
+    const btnDiv = document.createElement("div");
+    btnDiv.className = "btn";
+
+    const resumeBtn = document.createElement("button");
+    resumeBtn.style.backgroundColor = "blue";
+    resumeBtn.style.color = "beige";
+    resumeBtn.style.fontSize = "large";
+    resumeBtn.style.fontFamily = "fantasy";
+    resumeBtn.textContent = "Resume";
+
+    const projectsBtn = document.createElement("button");
+    projectsBtn.style.fontSize = "large";
+    projectsBtn.style.fontFamily = "fantasy";
+    projectsBtn.textContent = "Projects";
+
+    btnDiv.appendChild(resumeBtn);
+    btnDiv.appendChild(projectsBtn);
+
+    textDiv.appendChild(textH1);
+    textDiv.appendChild(textP1);
+    textDiv.appendChild(textH2);
+    textDiv.appendChild(btnDiv);
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "img";
+    const img = document.createElement("img");
+    img.src = "https://startbootstrap.github.io/startbootstrap-personal/assets/profile.png";
+    img.alt = "";
+
+    imgDiv.appendChild(img);
+
+    headerDiv.appendChild(textDiv);
+    headerDiv.appendChild(imgDiv);
+
+    headerContainer.appendChild(headerDiv);
+    header.appendChild(headerContainer);
+    document.body.appendChild(header);
+
+    const section = document.createElement("section");
+    const sectionH1 = document.createElement("h1");
+    sectionH1.textContent = "About Me";
+    const sectionP1 = document.createElement("p");
+    sectionP1.textContent = "My name is Start Bootstrap and I help brands grow.";
+    const sectionBr = document.createElement("br");
+    const sectionP2 = document.createElement("p");
+    sectionP2.textContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?";
+    const sectionIcon = document.createElement("div");
+    sectionIcon.className = "icon";
+    const twitterIcon = document.createElement("i");
+    twitterIcon.className = "bi bi-twitter";
+    const linkedinIcon = document.createElement("i");
+    linkedinIcon.className = "bi bi-linkedin";
+    const githubIcon = document.createElement("i");
+    githubIcon.className = "bi bi-github";
+    
+    sectionIcon.appendChild(twitterIcon);
+    sectionIcon.appendChild(linkedinIcon);
+    sectionIcon.appendChild(githubIcon);
+
+    section.appendChild(sectionH1);
+    section.appendChild(sectionP1);
+    section.appendChild(sectionBr);
+    section.appendChild(sectionP2);
+    section.appendChild(sectionIcon);
+
+    document.body.appendChild(section);
 });
